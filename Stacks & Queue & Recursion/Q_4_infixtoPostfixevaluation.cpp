bddf5822851774af4c7ae3d8914dc69cd6d81a85
Md Rayhanul Islam <73143090+rayhanul2008@users.noreply.github.com>
2022-11-16 12:26:06 +0600

    data structure lab

--- Stacks & Queue & Recursion/Q_4_infixtoPostfixevaluation.cpp
+++ Stacks & Queue & Recursion/Q_4_infixtoPostfixevaluation.cpp
@@ -1,36 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int getPriority(char arith){
-	if(arith == '+' || arith == '-'){
-		return 1;
-	}
-	else if(arith == '*' || arith == '/'){
-		return 2;
-	}
-	else if(arith == '^'){
-		return 3;
-	}
-	else if((arith >= 'a' && arith <= 'z') || (arith >= 'A' && arith <= 'Z') || (arith >= '0' && arith <= '9') || (arith == '.')){
+int getpriority(char ch){
+	if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') || (ch == '.')){
 		return 0;
 	}
+	else if(ch == '^') return 3;
+	else if(ch == '*' || ch == '/') return 2;
+	else if(ch == '+' || ch == '-') return 1;
 	return -1;
 }
 string infixtopostfix(string infix){
 	stack <char> stk;
 	string postfix = "";
-	int point = -1;
+
 	for(int i = 0; i < infix.size(); i++){
-		if(getPriority(infix[i]) == 0){
-			while(getPriority(infix[i]) == 0){
+		if(getpriority(infix[i]) == 0){
+			while(getpriority(infix[i]) == 0){
 				postfix += infix[i];
 				i++;
 			}
 			i--;
 			postfix += ',';
 		}
-		else if(infix[i] == '('){
-			stk.push(infix[i]);
-		}
+		else if(infix[i] == '(') stk.push(infix[i]);
 		else if(infix[i] == ')'){
 			while(!stk.empty() && stk.top() != '('){
 				postfix += stk.top();
@@ -40,14 +32,13 @@ string infixtopostfix(string infix){
 			stk.pop();
 		}
 		else{
-			int priority = getPriority(infix[i]);
-			while(!stk.empty() && stk.top() != '(' && priority <= getPriority(stk.top())){
+			int priority = getpriority(infix[i]);
+			while(!stk.empty() && stk.top() != '(' && priority <= getpriority(stk.top())){
 				postfix += stk.top();
 				postfix += ',';
 				stk.pop();
 			}
 			stk.push(infix[i]);
-
 		}
 	}
 	while(!stk.empty()){
@@ -55,13 +46,14 @@ string infixtopostfix(string infix){
 		postfix += ',';
 		stk.pop();
 	}
+	postfix.erase(postfix.end() - 1);
+	cout << "Infix To Postfix : " << endl;
 	cout << postfix << endl;
 	return postfix;
+
 }
 int main(){
 	string infix = "A+(180.5*C-(D/E^F)*G)*2.0";
 	string postfix = infixtopostfix(infix);
-	cout << postfix << endl;
-
 	return 0;
 }
